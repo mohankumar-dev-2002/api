ed57a970dbd042c79a8cb4c6e49e49257a0dfc89
mohankumar-dev-2002 <www.ghostrider125@gmail.com>
2023-06-22 16:49:31 +0530

    add api keys

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
 
-const apiKeys = ['key1', 'key2', 'key3'];
+const apiKeys = [
+'3ffg5h7j8k',
+'1a2b3c4d5e',
+'qwertyuiop',
+'9z8x7y6w5v',
+'6y7u8i9o0p',
+'asdfghjklz',
+'poiuytrewq',
+'mnbvcxzlkj',
+'0987654321',
+'lkjhgfdsaq'];
 
 const dummyUserData = [
   { id: 1, name: 'John Doe', email: 'johndoe@example.com', age: 30, city: 'New York' },
   { id: 2, name: 'Jane Smith', email: 'janesmith@example.com', age: 25, city: 'London' },
   { id: 3, name: 'Bob Johnson', email: 'bobjohnson@example.com', age: 40, city: 'Sydney' },
-  // Add more user objects with different properties
   { id: 4, name: 'Alice Brown', email: 'alicebrown@example.com', age: 35, city: 'Tokyo' },
   { id: 5, name: 'Michael Lee', email: 'michaellee@example.com', age: 28, city: 'Paris' },
 ];
 
 const authenticateApiKey = (req, res, next) => {
-  const apiKey = req.query.apiKey;
+  const apiKey = req.query.key;
 
   if (!apiKey || !apiKeys.includes(apiKey)) {
     return res.status(401).json({ error: 'Invalid API key' });
