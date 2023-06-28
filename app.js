7c8e375ae90e7a128c7167e657e6192673e9c795
root <iamroot@ubuntuLTS20.myguest.virtualbox.org>
2023-06-28 14:26:02 +0530

    cors unblock

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const faker = require('faker');
 const app = express();
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // You can also set other CORS headers if needed
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Set the Access-Control-Allow-Credentials header if your client makes requests with credentials (e.g., cookies)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 const apiKeys = [
   '3ffg5h7j8k',
